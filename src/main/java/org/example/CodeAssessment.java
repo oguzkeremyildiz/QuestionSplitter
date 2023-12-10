ff951d3f84c441c4ead1095875dff711b78f475e
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2023-12-10 19:20:10 +0300

    Initial Commit.

--- src/main/java/org/example/CodeAssessment.java
+++ src/main/java/org/example/CodeAssessment.java
@@ -3,32 +3,33 @@ package org.example;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class CodeAssessment {
 
-    private static ArrayList<Integer> findCandidates(ArrayList<Integer> possibilities, ArrayList<Integer> splits) {
+    private static ArrayList<Integer> findCandidates(ArrayList<Integer> possibilities, ArrayList<Integer> splits, HashMap<Integer, Integer> map) {
         ArrayList<Integer> candidates = new ArrayList<>();
         int i = 0;
         if (!splits.isEmpty()) {
-            i = splits.get(splits.size() - 1) + 1;
+            i = map.get(splits.get(splits.size() - 1)) + 1;
         }
         for (; i < possibilities.size(); i++) {
-            candidates.add(i);
+            candidates.add(possibilities.get(i));
         }
         return candidates;
     }
 
-    private static Pair<ArrayList<Integer>, Double> backtrack(ArrayList<Integer> splits, PointCalculator calculator, ArrayList<Integer> possibilities, ArrayList<String> codeLines) throws CloneNotSupportedException {
-        ArrayList<Integer> candidates = findCandidates(possibilities, splits);
+    private static Pair<ArrayList<Integer>, Double> backtrack(ArrayList<Integer> splits, PointCalculator calculator, ArrayList<Integer> possibilities, ArrayList<String> codeLines, HashMap<Integer, Integer> map) throws CloneNotSupportedException {
+        ArrayList<Integer> candidates = findCandidates(possibilities, splits, map);
         if (candidates.isEmpty() || calculator.refCodeSize() == splits.size()) {
             return new Pair<>((ArrayList<Integer>) splits.clone(), calculator.calculate(splits, codeLines));
         }
         Pair<ArrayList<Integer>, Double> pair = new Pair<>(null, (double) Integer.MIN_VALUE);
         for (Integer candidate : candidates) {
             splits.add(candidate);
-            Pair<ArrayList<Integer>, Double> p = backtrack(splits, calculator, possibilities, codeLines);
+            Pair<ArrayList<Integer>, Double> p = backtrack(splits, calculator, possibilities, codeLines, map);
             if (pair.getValue() < p.getValue()) {
                 pair = p.clone();
             }
@@ -49,7 +50,12 @@ public class CodeAssessment {
                     codeLines.add(source.nextLine());
                 }
                 source.close();
-                Pair<ArrayList<Integer>, Double> bestPair = backtrack(new ArrayList<>(), calculator, Splitter.split(codeLines), codeLines);
+                ArrayList<Integer> possibilities = Splitter.split(codeLines);
+                HashMap<Integer, Integer> map = new HashMap<>();
+                for (int j = 0; j < possibilities.size(); j++) {
+                    map.put(possibilities.get(j), j);
+                }
+                Pair<ArrayList<Integer>, Double> bestPair = backtrack(new ArrayList<>(), calculator, possibilities, codeLines, map);
                 splitLines.add(bestPair.getKey());
                 //System.out.println(listOfFiles[i].getName() + " is done with " + bestPair.getValue() + " accuracy.");
             }
---
+++ src/main/java/org/example/LineCalculator.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class LineCalculator extends BaseCalculator {
+
+    public LineCalculator(File refCode) throws FileNotFoundException {
+        super(refCode);
+    }
+
+    @Override
+    public double calculate(ArrayList<Integer> splits, ArrayList<String> file) {
+        double total = 0;
+        for (int i = 0; i < splits.size() - 1; i++) {
+            total -= Math.abs((splits.get(i + 1) - splits.get(i)) - refCodeList.get(i).size());
+        }
+        total -= Math.abs((file.size() - splits.get(splits.size() - 1)) - refCodeList.get(splits.size() - 1).size());
+        return total;
+    }
+}

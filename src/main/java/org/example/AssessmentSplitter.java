286d8555beb485393b134efe4bf131aff229a395
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2024-07-13 15:22:46 +0300

    Initial Commit.

--- src/main/java/org/example/AssessmentSplitter.java
+++ src/main/java/org/example/AssessmentSplitter.java
@@ -6,35 +6,62 @@ import java.util.*;
 
 public class AssessmentSplitter {
 
-    public static void splitFiles(String folderName) throws IOException {
-        BufferedWriter outfile = null;
+    public static void splitFiles(String folderName, String toDirectory) throws IOException {
+        BufferedWriter outfile;
         File folder = new File(folderName);
         File[] listOfFiles = folder.listFiles();
         for (int i = 0; i < Objects.requireNonNull(listOfFiles).length; i++) {
             if (listOfFiles[i].isFile()) {
+                LinkedList<ArrayList<String>> lists = new LinkedList<>();
+                //System.out.println(listOfFiles[i].getName());
                 Scanner scanner = new Scanner(listOfFiles[i]);
-                int splits = 0;
-                String filaName = listOfFiles[i].getName();
-                scanner.nextLine();
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    if (line.startsWith("/** ASSESSMENT")) {
-                        splits++;
-                        if (outfile != null) {
-                            outfile.close();
-                        }
-                        OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(filaName + "-" + splits), StandardCharsets.UTF_8);
-                        outfile = new BufferedWriter(writer);
-                        for (int j = 0; j < 3; j++) {
-                            scanner.nextLine();
-                        }
-                        line = scanner.nextLine();
-                    }
-                    outfile.write(line);
-                    outfile.newLine();
+                String fileName = listOfFiles[i].getName();
+                String line = scanner.nextLine();
+                while (!line.contains("ASSESSMENT")) {
+                    line = scanner.nextLine();
                 }
-                outfile.close();
+                for (int j = 0; j < 3; j++) {
+                    scanner.nextLine();
+                }
+                line = scanner.nextLine();
+                lists.add(new ArrayList<>());
+                while (scanner.hasNextLine()) {
+                    while (!line.contains("ASSESSMENT")) {
+                        if (!line.isEmpty()) {
+                            lists.getLast().add(line);
+                        }
+                        if (scanner.hasNextLine()) {
+                            line = scanner.nextLine();
+                        } else {
+                            break;
+                        }
+                    }
+                    if (!scanner.hasNextLine()) {
+                        break;
+                    }
+                    for (int j = 0; j < 3; j++) {
+                        scanner.nextLine();
+                    }
+                    if (scanner.hasNextLine()) {
+                        line = scanner.nextLine();
+                    } else {
+                        break;
+                    }
+                    lists.add(new ArrayList<>());
+                }
                 scanner.close();
+                if (!lists.getLast().isEmpty() && lists.getLast().get(lists.getLast().size() - 1).trim().equals("}")) {
+                    lists.getLast().remove(lists.getLast().size() - 1);
+                }
+                for (int j = 0; j < lists.size(); j++) {
+                    OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(toDirectory + "/" + fileName + "-" + j), StandardCharsets.UTF_8);
+                    outfile = new BufferedWriter(writer);
+                    for (int k = 0; k < lists.get(j).size(); k++) {
+                        outfile.write(lists.get(j).get(k));
+                        outfile.newLine();
+                    }
+                    outfile.close();
+                }
             }
         }
     }
